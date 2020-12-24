1f15a947cf3cd2cfdbdeec907cbd7718e2aeda7a
Dmitrii <zeionara@gmail.com>
2020-12-24 20:37:56 +0300

    Fixed package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.1
+// swift-tools-version:5.3
 // Generated automatically by Perfect Assistant 2
 // Date: 2018-03-28 18:54:02 +0000
 import PackageDescription
@@ -11,7 +11,7 @@ let package = Package(
 	dependencies: [
 		.package(name: "PerfectNet", url: "https://github.com/PerfectlySoft/Perfect-Net", from: "3.1.2"),
 		.package(name: "PerfectHTTP", url: "https://github.com/PerfectlySoft/Perfect-HTTP", from: "3.0.12"),
-		.package(name: "Perfect-CZlib-src", url: "https://github.com/PerfectlySoft/Perfect-CZlib-src", from: "0.0.0")
+		.package(name: "PerfectCZlib", url: "https://github.com/PerfectlySoft/Perfect-CZlib-src", from: "0.0.0")
 	],
 	targets: [
 		.target(name: "PerfectCHTTPParser", dependencies: []),
